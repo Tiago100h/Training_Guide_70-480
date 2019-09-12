b8730f04247932eafe60266d906c10dd22f0151e
Tiago Giuliatte <tiagogiuliatte.p@gmail.com>
2019-09-12 17:47:21 -0300

    Chapter 3: Getting started with JavaScript

---
+++ Chapter 3 Getting started with JavaScript/Lesson 2 Writing, testing, and debugging JavaScript/Scripts/tests.js
@@ -0,0 +1,3 @@
+QUnit.test("A Hello World Test", function (assert) {
+    assert.equal(greeting, "Hello World", "Expect greeting of Hello World");
+});
---
+++ Chapter 3 Getting started with JavaScript/Practice exercises/WebCalculator/Scripts/tests.js
@@ -0,0 +1,62 @@
+QUnit.module('Calculator Test Suite', {
+    beforeEach: initialize
+});
+
+QUnit.test("Button Click Test", function (assert) {
+    var buttonQuantity = 10;
+    for (var i = 0; i < buttonQuantity; i++) {
+        var btn = document.getElementById('btn' + i);
+        btn.click();
+        var result = txtInput.value[txtInput.value.length - 1];
+        var expected = String(i);
+        assert.equal(result, expected, 'Expected value: ' + expected + ' Actual value: ' + result);
+        var expectedLength = i < 2 ? 1 : i;
+        assert.equal(txtInput.value.length, expectedLength, 'Expected string length: ' + expectedLength + ' Actual value: ' + txtInput.value.length);
+    }
+});
+
+QUnit.test("Add Test", function (assert) {
+    txtInput.value = '10';
+    txtResult.value = '20';
+    var btnPlus = document.getElementById('btnPlus');
+    btnPlus.click();
+    var expected = '30';
+    assert.equal(txtResult.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtResult.value);
+    expected = '0';
+    assert.equal(txtInput.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtInput.value);
+});
+
+QUnit.test("Subtract Test", function (assert) {
+    txtInput.value = '10';
+    txtResult.value = '20';
+    var btnMinus = document.getElementById('btnMinus');
+    btnMinus.click();
+    var expected = '10';
+    assert.equal(txtResult.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtResult.value);
+    expected = '0';
+    assert.equal(txtInput.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtInput.value);
+});
+
+QUnit.test("Clear Entry Test", function (assert) {
+    txtInput.value = '10';
+    var btnClearEntry = document.getElementById('btnClearEntry');
+    btnClearEntry.click();
+    var expected = '0';
+    assert.equal(txtInput.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtInput.value);
+});
+
+QUnit.test("Clear Test", function (assert) {
+    txtInput.value = '10';
+    txtResult.value = '20';
+    var btnClear = document.getElementById('btnClear');
+    btnClear.click();
+    var expected = '0';
+    assert.equal(txtInput.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtInput.value);
+    assert.equal(txtResult.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtResult.value);
+});
+
+QUnit.test("Initialize Test", function (assert) {
+    var expected = '0';
+    assert.equal(txtInput.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtInput.value);
+    assert.equal(txtResult.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtResult.value);
+});
