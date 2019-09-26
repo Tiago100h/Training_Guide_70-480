6a421b789f714df0a17c386c83f826f4109e2598
Tiago Giuliatte Pereira <tiagogiuliatte.p@gmail.com>
2019-09-26 18:04:29 -0300

    Chapter 16: Offline web applications

--- Practice Exercises/ContactBook/Scripts/ContactsLibrary.js
+++ Practice Exercises/ContactBook/Scripts/ContactsLibrary.js
@@ -1,5 +1,9 @@
 /// <reference path="jquery-3.4.1.min.js" />
 
+window.indexedDB = window.indexedDB || window.mozIndexedDB ||window.webkitIndexedDB || window.msIndexedDB;
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction;
+window.IDBCursor = window.IDBCursor || window.webkitIDBCursor;
+
 $(document).ready(function () {
     contactsNamespace.initialize();
 });
@@ -7,11 +11,25 @@ $(document).ready(function () {
 (function () {
     this.contactsNamespace = this.contactsNamespace || {};
     var ns = this.contactsNamespace;
+    var db;
     var currentRecord;
 
     ns.initialize = function () { 
         $('#btnSave').on('click', ns.save);
-        ns.display();
+        var request = indexedDB.open('Chapter16', 1);
+
+        request.onupgradeneeded = function (response) {
+            var options = {
+                keypath: 'id',
+                autoIncrement: true
+            };
+            response.currentTarget.result.createObjectStore("contacts", options);
+        };
+
+        request.onsuccess = function (response) {
+            db = request.result;
+            ns.display();
+        };
     };
 
     function retrieveFromStorage() {
@@ -27,18 +45,35 @@ $(document).ready(function () {
         };
         displayCurrentRecord();
 
-        var results = retrieveFromStorage();
-        bindToGrid(results);
+        var trans = db.transaction('contacts', 'readonly');
+        var request = trans.objectStore("contacts").openCursor();
+        var results = [];
+
+        request.onsuccess = function (response) {
+            var cursor = response.target.result;
+
+            if (!cursor) {
+                bindToGrid(results);
+                return;
+            }
+
+            results.push({
+                key: cursor.key,
+                contact: cursor.value
+            });
+            cursor.continue();
+        };
     };
 
     function bindToGrid(results) {
         var html = '';
 
         for (var i = 0; i < results.length; i++) {
-            var contact = results[i];
+            var key = results[i].key;
+            var contact = results[i].contact;
             html += '<tr><td>' + contact.email + '</td>';
             html += '<td>' + contact.firstName + ' ' + contact.lastName + '</td>';
-            html += '<td><a class="edit" href="javascript:void(0)" data-key=' + i + '>Edit</a></td></tr>'
+            html += '<td><a class="edit" href="javascript:void(0)" data-key=' + key + '>Edit</a></td></tr>'
         }
         html = html || '<tr><td colspan="3">No recrods available</td></tr>';
         $('#contacts tbody').html(html);
@@ -47,13 +82,18 @@ $(document).ready(function () {
 
     ns.loadContact = function () {
         var key = parseInt($(this).attr('data-key'));
-        var results = retrieveFromStorage();
-        $('#currentAction').html('Edit Contact');
-        currentRecord = {
-            key: key,
-            contact: results[key]
+        var trans = db.transaction('contacts', 'readonly');
+        var store = trans.objectStore("contacts");
+        var request = store.get(key);
+
+        request.onsuccess = function (response) {
+            $('#currentAction').html('Edit Contact');
+            currentRecord = {
+                key: key,
+                contact: response.target.result
+            };
+            displayCurrentRecord();
         };
-        displayCurrentRecord();
     };
 
     function displayCurrentRecord() {
@@ -71,17 +111,13 @@ $(document).ready(function () {
         contact.email = $('#email').val();
         contact.phoneNumber = $('#phoneNumber').val();
 
-        var results = retrieveFromStorage();
+        var trans = db.transaction('contacts', 'readwrite');
+        var contacts = trans.objectStore("contacts");
+        var request = currentRecord.key != null ? contacts.put(contact, currentRecord.key) : contacts.add(contact);
 
-        if (currentRecord.key != null) {
-            results[currentRecord.key] = contact;
-        }
-        else {
-            results.push(contact);
-        }
-
-        localStorage.setItem('contacts', JSON.stringify(results));
-        ns.display();
+        request.onsuccess = function (response) {
+            ns.display();
+        };
     };
     
 })();
